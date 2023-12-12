1d5c6caa4023976d1e73ff88a0879feb28802edb
Karina <karina.kvych@gmail.com>
2023-12-12 11:09:11 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,42 +1,9 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-console.log("Hello World")
-app.get("/", function(req, res) {
-    res.send("Hello Express");
-  });
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
 
+// console.log("Hello World");
+module.exports = app;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
