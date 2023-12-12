dcf6d226e099acab6598b64203defbf9f280d33a
Karina <karina.kvych@gmail.com>
2023-12-12 11:01:42 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
-app.get("Hello Express")
 console.log("Hello World")
+app.get("/",function(req,res){
+    res.send("Hello Express")
+})
 
 
 
